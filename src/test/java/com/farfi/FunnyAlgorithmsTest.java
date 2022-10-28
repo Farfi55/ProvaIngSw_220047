32df256ad800635282c7b0abff96cd22a1b7c330
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-10-28 15:59:57 +0200

    first test on stringToIntConverter

---
+++ src/test/java/com/farfi/FunnyAlgorithmsTest.java
@@ -0,0 +1,28 @@
+package com.farfi;
+
+import org.junit.Assert;
+import org.junit.Before;
+
+public class FunnyAlgorithmsTest
+{
+	private FunnyAlgorithms fn;
+
+	@Before
+	public void BeforeTest()
+	{
+		fn = new FunnyAlgorithms();
+	}
+
+
+	@org.junit.Test
+	public void stringToIntConverterShouldParseCorrectly()
+	{
+		String[] inputs = new String[]{"-3", "500", "-10", "32767"};
+		int[] expectedOutputs = new int[]{-3, 500, -10, 32767};
+		for (int i = 0; i < inputs.length; i++) {
+			String input = inputs[i];
+			int expectedOutput = expectedOutputs[i];
+			Assert.assertEquals(fn.stringToIntConverter(input), expectedOutput);
+		}
+	}
+}
